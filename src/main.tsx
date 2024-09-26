9d2f174213cc8d2e47d2dc88c07fcb2f68d4c98b
Umang Patel <umangpatel3105@gmail.com>
2024-09-26 11:54:34 +0530

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './assets/styles/tailwind.css';
+import './assets/styles/global.scss';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
